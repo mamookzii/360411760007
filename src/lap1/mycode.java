6b699dfe248cbca8ff612e6ce40c6011b24fc4bf
mamookzii <mookpakphanang@gmail.com>
2018-06-26 15:07:32 +0700

    test upload

---
+++ src/lap1/mycode.java
@@ -0,0 +1,20 @@
+package lap1;
+
+public class mycode {
+  //main
+  public static void main(String[] args) {
+      System.out.println("Halle World");
+      System.out.println("Naputsorn Phanchai");
+      System.out.println("360411760007");
+      System.out.println("MIT421");
+      System.out.println("RMUTSV");
+
+
+
+
+  }//main
+
+
+
+
+}//class
