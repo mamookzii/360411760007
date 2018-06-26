6c12303eda74d2b6ced63462d53b5f0502d115c6
mamookzii <mookpakphanang@gmail.com>
2018-06-26 15:32:07 +0700

    test uplode2

---
+++ src/lap1/mycode2.java
@@ -0,0 +1,12 @@
+package lap1;
+
+public class mycode2 {
+  //main
+  public static void main(String[] args) {
+      System.out.println("Mamookzii");
+      System.out.println("360411760007");
+
+  }//main
+
+
+}
