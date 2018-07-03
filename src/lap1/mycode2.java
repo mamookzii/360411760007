c724a11317d3e182dfda7425a22dc115e30ea5b2
Naputsorn Phanchai <mookpakphanang@gmail.com>
2018-07-03 13:59:18 +0700

    test git account

--- src/lap1/mycode2.java
+++ src/lap1/mycode2.java
@@ -5,7 +5,7 @@ public class mycode2 {
   public static void main(String[] args) {
       System.out.println("Mamookzii");
       System.out.println("360411760007");
-
+....  System.out.println("Mook");
   }//main
 
 
